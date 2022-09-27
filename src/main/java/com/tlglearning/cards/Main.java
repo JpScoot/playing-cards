7c56323ca313a1800e926d79b3506123cd9aecc1
JpScoot <jamespjones0714@gmail.com>
2022-09-27 14:02:28 -0700

    ColorFirst as a local class in Main().

--- src/main/java/com/tlglearning/cards/Main.java
+++ src/main/java/com/tlglearning/cards/Main.java
@@ -9,33 +9,34 @@ import java.util.Random;
 public class Main {
 
   public static void main(String[] args) {
+     class ColorFirst implements Comparator<Card> {
+
+      @Override
+      public int compare(Card card1, Card card2) {
+        int comparison = card1.suit().color().compareTo(card2.suit().color());
+        if (comparison == 0) {
+          comparison = card1.suit().compareTo(card2.suit());
+          if (comparison == 0) {
+            comparison = -card1.rank().compareTo(card2.rank());
+          }
+        }
+        return comparison;
+      }
+    }
+
     Deck deck = new Deck(); // create an instance of Deck
     System.out.println(deck); // print deck.
 
     Random rng = new SecureRandom();  // create an acceptable source of randomness.
-    deck.Shuffle(rng); //  shuffle the deck, using the created source of randomness
+    deck.Shuffle(rng); //  shuffle the deck, using the created source of randomness.
     System.out.println(deck); // print the string representation of the deck after shuffling.
+
     deck.sort();
     System.out.println(deck);
     deck.sort(new ColorFirst());
     System.out.println(deck);
   }
 
-  // static nested class
-  private static class ColorFirst implements Comparator<Card> {
-
-    @Override
-    public int compare(Card card1, Card card2) {
-      int comparison = card1.suit().color().compareTo(card2.suit().color());
-      if (comparison == 0) {
-        comparison = card1.suit().compareTo(card2.suit());
-        if (comparison == 0) {
-          comparison = -card1.rank().compareTo(card2.rank());
-        }
-      }
-      return comparison;
-    }
-  }
 
 }
 
